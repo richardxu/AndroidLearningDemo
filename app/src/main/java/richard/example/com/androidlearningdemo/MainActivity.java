24e2a6d53bc103a3cd5fca69d2e5aaf27d9822a0
richardxu <xulican@gmail.com>
2016-11-07 18:13:35 +0800

    Initial commit

---
+++ app/src/main/java/richard/example/com/androidlearningdemo/MainActivity.java
@@ -0,0 +1,54 @@
+package richard.example.com.androidlearningdemo;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+
+import richard.example.com.androidlearningdemo.context.ContextActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    public final static String TAG  = "richard";
+
+    public Button buttonActivity;
+    public Button buttonContext;
+//    public ListView mlistView;
+//    public Context context = this;
+ //   public Intent intent;// = new Intent(MainActivity.this, ContextActivity.class);
+    public Intent intent  = new Intent(MainActivity.this, ContextActivity.class);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonActivity = (Button) findViewById(R.id.button_activity);
+        buttonActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "============== buttongActivity clicked ============= ");
+            }
+        });
+
+        buttonContext = (Button) findViewById(R.id.button_context);
+
+
+        buttonContext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "============== button Context clicked ============= ");
+                //intent  = new Intent(MainActivity.this, ContextActivity.class);
+                startActivity(intent);
+            }
+        });
+
+//        mlistView = (ListView) findViewById(R.id.listview_activities);
+
+
+    }
+}
---
+++ app/src/main/java/richard/example/com/androidlearningdemo/context/ContextActivity.java
@@ -0,0 +1,17 @@
+package richard.example.com.androidlearningdemo.context;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2016/11/7.
+ */
+
+public class ContextActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
