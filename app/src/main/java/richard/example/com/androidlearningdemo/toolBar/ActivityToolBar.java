1ebcb6c3a6369f4bc6561fd075569f7e6c783fbf
richardxu <xulican@gmail.com>
2016-11-15 15:51:40 +0800

    定制 tool bar

--- app/src/main/java/richard/example/com/androidlearningdemo/toolBar/ActivityToolBar.java
+++ app/src/main/java/richard/example/com/androidlearningdemo/toolBar/ActivityToolBar.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.widget.TextView;
 
 import richard.example.com.androidlearningdemo.R;
 
@@ -23,6 +24,17 @@ public class ActivityToolBar extends AppCompatActivity {
         // Sets the Toolbar to act as the ActionBar for the Activity window
         // Make sure the toolbar exists in the activity and is not null
         setSupportActionBar(toolbar);
+
+        //Display icon in the toolbar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+
+
+        //Remove default title text
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        //Get access to the custom title view
+        TextView mTitle = (TextView) toolbar.findViewById(R.id.toolbar_title);
     }
 
     //Menu icons are inflated just as they were with actionbar
